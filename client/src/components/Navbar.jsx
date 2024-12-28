fd8c1354f8748285c822a5ecacd63b4e026c198f
BhaskarT18 <bhaskartikale@gmail.com>
2024-12-28 18:02:10 +0530

    changes svg

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -58,17 +58,29 @@ const Navbar = () => {
     <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
       <nav className="flex justify-between items-center py-6">
         <NavLink to="/" className="flex items-center gap-2 text-2xl text-black">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="29"
-            height="30"
-            viewBox="0 0 29 30"
-            fill="none"
-            aria-label="Job Portal Logo"
-          >
-            <circle cx="12.0143" cy="12.5143" r="12.0143" fill="#3575E2" fillOpacity="0.4" />
-            <circle cx="16.9857" cy="17.4857" r="12.0143" fill="#3575E2" />
-          </svg>
+        <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="29"
+  height="30"
+  viewBox="0 0 29 30"
+  fill="none"
+  aria-label="Job Posting Icon"
+>
+ <circle cx="14.5" cy="15" r="14" fill="#3575E2" fill-opacity="0.4" />
+
+  <circle cx="14.5" cy="15" r="10" fill="#3575E2" />
+ 
+  <path
+    d="M10 9H19C19.5523 9 20 9.44772 20 10V21C20 21.5523 19.5523 22 19 22H10C9.44772 22 9 21.5523 9 21V10C9 9.44772 9.44772 9 10 9Z"
+    fill="white"
+  />
+  <path
+    d="M12 11H17V13H12V11ZM12 15H17V17H12V15ZM12 19H15V21H12V19Z"
+    fill="#3575E2"
+  />
+</svg>
+
+
           <span>Job Portal</span>
         </NavLink>
         {/* Nav items for large device */}
